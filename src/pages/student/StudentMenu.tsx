fe8d6670947fef158fefd4fec51b76c9bf1c67b6
janiruchit432 <janiruchit432@gmail.com>
2026-03-23 09:52:09 +0530

    Create StudentMenu.tsx

---
+++ src/pages/student/StudentMenu.tsx
@@ -0,0 +1,8 @@
+export default function StudentMenu() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Student Menu</h2>
+      <p>This is the student menu page.</p>
+    </div>
+  );
+}
